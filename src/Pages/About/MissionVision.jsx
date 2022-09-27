20ccd5efd7dcf5238b3a8ac6be8193c767d75a3f
neelsaspara29 <106027223+neelsaspara29@users.noreply.github.com>
2022-09-27 21:26:14 +0530

    about changes

---
+++ src/Pages/About/MissionVision.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+
+const MissionVision = () => {
+    return (
+        <>
+                       	<div id="interior_content">
+
+<div id="interior_header_banner"><span id="bnrinst_2_453" ><img src="/Assets/about/fs_453.jpg"  alt='Hello' width="1439" height="363"  /></span>
+</div>
+<div class="wrapper">
+                <div id="content" class="rightpad"><h1 class="gendocsPageTitle">Mission</h1><p>The Greater Fort Lauderdale Chamber of Commerce is the largest and oldest business organization in Broward County. Established in 1910, the Chamber has a membership base of 1,252 companies that employ more than 500,000 employees.&nbsp;The Chamber is&nbsp;committed to serving the business community and helping &nbsp;promote future economic growth and development in South Florida. To that end, we provide members with a host of networking opportunities. In addition to weekly, monthly and annual events, we currently work with seven "leads groups," which connect businesses and individuals with prospective clients. The Chamber takes an active role in issues affecting the Greater Fort Lauderdale area, such as sea level rise and coastal resiliency, community-based real estate, regionalism and access to capital. We also take a delegation to Capitol Hill to urge lawmakers to take action on an assortment of action items every two years in our <a href="http://washingtonsummit.org/" target="_blank" rel="noopener noreferrer">Washington Summit</a> trip.</p>
+                <hr />
+                <h1 class="gendocsPageTitle">Vision</h1><p>The Greater Fort Lauderdale Chamber of Commerce is the largest and oldest business organization in Broward County. Established in 1910, the Chamber has a membership base of 1,252 companies that employ more than 500,000 employees.&nbsp;The Chamber is&nbsp;committed to serving the business community and helping &nbsp;promote future economic growth and development in South Florida. To that end, we provide members with a host of networking opportunities. In addition to weekly, monthly and annual events, we currently work with seven "leads groups," which connect businesses and individuals with prospective clients. The Chamber takes an active role in issues affecting the Greater Fort Lauderdale area, such as sea level rise and coastal resiliency, community-based real estate, regionalism and access to capital. We also take a delegation to Capitol Hill to urge lawmakers to take action on an assortment of action items every two years in our <a href="http://washingtonsummit.org/" target="_blank" rel="noopener noreferrer">Washington Summit</a> trip.</p>
+<p>To learn more about the Chamber, contact our membership team <a href="forms/membership-inquiry/">here.</a></p>
+</div>
+                
+                <div id="sidebar">
+        <div id="sub_menu">
+<div id="sub_menu_title">About</div>
+
+<ul class="sub_menu"  >
+<li class="item1" ><a class="first" href="pages/about-us/">About Us </a></li>
+<li class="item2" ><a href="pages/the-chamber-s-mission-and-vision-for-the-future/">Mission & Vision </a></li>
+<li class="item3 " ><a class=""   href="index.php?submenu=boardofdirectors&amp;src=directory&amp;view=staff&amp;srctype=board_lister">Board of Directors </a>
+<ul class="" >
+<li class="item1" ><a class="first last" href="pages/board-of-directors-library/">BOD Library </a></li>
+</ul>
+</li>
+<li class="item4" ><a href="staff-directory-redesign/ceo/dan-lindblade-cae/">Meet the CEO </a></li>
+<li class="item5" ><a href="staff-directory-redesign/">Meet the Staff </a></li>
+<li class="item6" ><a class="last" href="pages/join-our-team/">Join our Team </a></li>
+</ul>
+</div>
+
+        <div class="sidebanner"><span id="bnrinst_3_535" ><a class="fba_links" rel="nofollow" href="fba/td-bank/535/" ><img src="/Assets/Slider/fourth.jpg" width="350" height="300"  alt="TD Bank"  /></a></span>
+</div>
+        <div class="sidebanner"><span id="bnrinst_4_536" ><a class="fba_links" rel="nofollow" href="fba/buchanan/536/"><img src="/Assets/Slider/fourth.jpg" width="350" height="300" alt="Buchanan"  /></a></span>
+</div>
+        <div class="sidebanner"><span id="bnrinst_4_536" ><a class="fba_links" rel="nofollow" href="fba/buchanan/536/"><img src="/Assets/Slider/fourth.jpg" width="350" height="300" alt="Buchanan"  /></a></span>
+</div>
+    </div>
+                
+                
+                
+</div>
+
+</div>
+        </>
+    )
+}
+
+export default MissionVision;
