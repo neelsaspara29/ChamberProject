a6d6ec41b2c2a56c80dedc64307a0604a1c68aa9
neelsaspara29 <106027223+neelsaspara29@users.noreply.github.com>
2022-10-05 11:47:10 +0530

    changes in about bhavnagar

--- src/Pages/About/Bhavnagar.jsx
+++ src/Pages/About/Bhavnagar.jsx
@@ -6,130 +6,65 @@ function Bhavnagar() {
       <div>
         <h2 className="text-danger uppercase text-center my-4">Bhavnagar</h2>
       </div>
-      <div className="w-4/5 m-auto">
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam,
-          qui. Cumque, amet deserunt quisquam dolore eius tempore quo nam
-          eligendi nemo quae! Doloremque aliquid laboriosam tenetur soluta
-          voluptatem deleniti ipsum quo? Enim quis labore reiciendis modi,
-          veniam, ipsam at nihil fuga autem porro, beatae facere ad voluptas
-          ipsum quidem fugiat excepturi natus sapiente illum velit aperiam
-          repellat esse blanditiis. Quo. Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Laborum itaque quia quos. Cum soluta facilis
-          doloribus nihil porro non nam ducimus quod labore modi ut magni
-          incidunt harum vel totam, quam mollitia reprehenderit culpa amet eos!
-          Libero nulla dignissimos molestiae. Lorem ipsum dolor sit amet
-          consectetur, adipisicing elit. Est dolore reprehenderit, possimus
-          maxime enim ipsa deleniti vitae! Asperiores adipisci, ullam aut maxime
-          dicta aliquam tenetur ad iusto. Accusamus, quibusdam harum pariatur
-          veniam possimus quis quae iusto atque ad in, iure ea? Earum magni
-          praesentium consequatur beatae ipsa culpa animi recusandae saepe dicta
-          quisquam quas odio expedita, a ut sunt, natus et ab temporibus illum
-          nisi provident impedit placeat sequi pariatur. Iste ipsa eaque
-          accusamus. Labore facilis beatae sunt autem ullam officia, animi
-          incidunt eius? Tempora necessitatibus iure illum pariatur odit
-          officiis adipisci dicta ullam. Saepe earum fuga eveniet iste
-          temporibus.
-        </p>
-        <div className="my-6">
-          <h2 className="text-danger uppercase">
-            Industries Located in bhavnagar
-          </h2>
-        </div>
-        <div>
-          <div className="d-flex flex-wrap justify-between  mt-14">
-            <div className="industry_bhavnagar">
-              <img
-                src="/Assets/bhavnagr/chem.jpg"
-                style={{ width: "23rem", height: "23rem" }}
-                alt=""
-              />
-            </div>
-            <div className="w-2/3 bhannagar_industry">
-              <div className="text-green-500 uppercase industry_header">
-                <h3>chemical industry</h3>
-              </div>
-              <div>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                blanditiis alias cupiditate tempora? Ad, nihil repellat labore
-                nemo odit pariatur tempore ea quisquam autem facere. Excepturi
-                voluptate vero tenetur corporis maiores temporibus accusantium
-                assumenda nihil exercitationem, aspernatur similique magni.
-                Illum? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eos amet nemo, debitis rem vitae quasi dolorem. Sunt,
-                asperiores. Corrupti similique quia voluptates, earum quisquam
-                porro nostrum id ab, eligendi rem molestiae. Molestias obcaecati
-                libero quam ratione voluptate. Temporibus culpa, dolores,
-                nostrum voluptate ea fugiat rerum natus, quam similique
-                veritatis explicabo! Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Ratione, blanditiis rem aut porro provident
-                suscipit et explicabo voluptatibus nemo similique aliquam,
-                beatae placeat quisquam reprehenderit assumenda cupiditate! Vel,
-                totam aliquam?Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Molestias omnis tenetur facilis. Unde error
-                deserunt obcaecati eveniet beatae, consectetur quos, eius
-                asperiores veritatis doloribus itaque quia magni totam libero!
-                Iste pariatur expedita neque alias nesciunt nemo, aperiam
-                voluptatibus placeat officia quasi fugit laboriosam, sit
-                perspiciatis maiores corrupti nostrum. Molestias quas
-                aspernatur, assumenda fuga quia magni ratione aut praesentium
-                nam libero amet molestiae velit tempore vero ipsum facere, cum
-                beatae corrupti. Lorem ipsum dolor sit amet consectetur,
-                adipisicing elit. Incidunt ea corporis animi debitis temporibus
-                distinctio delectus, consequatur illum natus veniam labore minus
-                nihil. Id consectetur molestiae totam architecto veritatis
-                aliquid fugit natus! Voluptatum omnis molestiae maiores
-                excepturi ab hic.
-              </div>
-            </div>
-          </div>
-          <div className="d-flex flex-wrap justify-between  mt-14">
-            <div>
-              <img
-                src="/Assets/bhavnagr/text.jpg"
-                style={{ width: "23rem", height: "23rem" }}
-                alt=""
-              />
-            </div>
-            <div className="w-2/3 bhannagar_industry">
-              <div className="text-green-500 uppercase industry_header">
-                <h3>texttile industry</h3>
-              </div>
-              <div>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam
-                blanditiis alias cupiditate tempora? Ad, nihil repellat labore
-                nemo odit pariatur tempore ea quisquam autem facere. Excepturi
-                voluptate vero tenetur corporis maiores temporibus accusantium
-                assumenda nihil exercitationem, aspernatur similique magni.
-                Illum? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Eos amet nemo, debitis rem vitae quasi dolorem. Sunt,
-                asperiores. Corrupti similique quia voluptates, earum quisquam
-                porro nostrum id ab, eligendi rem molestiae. Molestias obcaecati
-                libero quam ratione voluptate. Temporibus culpa, dolores,
-                nostrum voluptate ea fugiat rerum natus, quam similique
-                veritatis explicabo! Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Ratione, blanditiis rem aut porro provident
-                suscipit et explicabo voluptatibus nemo similique aliquam,
-                beatae placeat quisquam reprehenderit assumenda cupiditate! Vel,
-                totam aliquam?Lorem, ipsum dolor sit amet consectetur
-                adipisicing elit. Molestias omnis tenetur facilis. Unde error
-                deserunt obcaecati eveniet beatae, consectetur quos, eius
-                asperiores veritatis doloribus itaque quia magni totam libero!
-                Iste pariatur expedita neque alias nesciunt nemo, aperiam
-                voluptatibus placeat officia quasi fugit laboriosam, sit
-                perspiciatis maiores corrupti nostrum. Molestias quas
-                aspernatur, assumenda fuga quia magni ratione aut praesentium
-                nam libero amet molestiae velit tempore vero ipsum facere, cum
-                beatae corrupti. Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Quo repudiandae similique porro dignissimos.
-                Repellendus nam illum necessitatibus eum tempore veritatis
-                veniam reiciendis explicabo nobis temporibus velit magni,
-                pariatur non adipisci, unde, doloremque et ipsa obcaecati
-                exercitationem error amet tempora!
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="w-4/5 m-auto pub flow-root">
+      <div class="city-box1">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLXu9bNsbC6WqRiox0sEfSzb-QBiXtrBFZGHO2CXac&s" alt="Surat By Road" />
+                            <h3>Road: </h3>
+                            <p>Auto Rickshaws and Electronic Rickshaws are the common means of transport that are the most preferred after the bus. These are mostly preferred for short distances and sometimes for long distances. The auto fare charges are
+                                quite fair and genuine and commonly run by the meter. Here, you can even find some auto-rickshaw at night. Most of the drivers are affable and love chatting, and many of them can cope up to speak Hindi apart from the local
+                                language Gujarati. They can be an outstanding guide to the city and cater to you with a lot of knowledge.</p>
+                        </div>
+
+                        <div class="city-box2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLXu9bNsbC6WqRiox0sEfSzb-QBiXtrBFZGHO2CXac&s" alt="Private vehicles in Surat" />
+                            <h3> Vehicles in Surat: </h3>
+                            <p>If you are thinking of relocating here with your family permanently in Surat, it is best to purchase a vehicle. Purchase any of your favorite types of vehicles here that are in your budget too. Many car dealers and showrooms
+                                are selling used/second-hand cars and the newt cars in the city. Moreover, if you already have a vehicle and are relocating to Surat. Then you can take the help of the thepackersmovers.com to relocate your automobile to
+                                here. </p>
+                        </div>
+
+                        <div class="city-box1">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLXu9bNsbC6WqRiox0sEfSzb-QBiXtrBFZGHO2CXac&s" alt="Bus Service in Surat" />
+                            <h3>Bus Service: </h3>
+                            <p>Bus service is the backbone of Surat Transportation. The majority of people are more dependent on public transport than any other means. As compared to Delhi, the surat has well-developed public transport. Surat Municipal Corporation
+                                has established a benchmark in achieving the difficult task of getting more and more users of autos and two-wheelers to go for city buses. An amalgamation of savvy pricing and smart technology has meant that Surat Sitilink
+                                Ltd, initiated less than a year before by the municipal body, has thrived in attracting 94% of passengers, many of whom were primarily using auto-rickshaws and two-wheelers. Surat Municipal Corporation was chosen for the
+                                best city best service award by the Centre at the Urban mobility conference in Hyderabad. Gujarat government has also initiated a fleet of 25 electric buses, plying only in the main city area covering 29 km. It has done
+                                to rectify vehicular pollution in mobbed areas. Besides this, the bus fare is at Rs. 4 as the minimum ticket is quite fair and genuine, competing to polluting shared auto-rickshaws.
+                            </p>
+                        </div>
+
+                        <div class="city-box2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLXu9bNsbC6WqRiox0sEfSzb-QBiXtrBFZGHO2CXac&s" alt="Railway Service in Surat" />
+                            <h3>Railway Service: </h3>
+                            <p>The Surat Railways Station is an A-1- category railway station of Western Railway station of Indian Railways serving Surat, beside Gothangam, Kosad, Utran, Udhna Junction, Bhestan, Niyol, and Sachin. The Indian Railway Catering
+                                and Tourism Corporation classified convenience as the best large station in India based on cleanliness. It has been linked to various stations in India. Easily accessible, computerized train control charting system, interactive
+                                touch screen kiosk, L.E.D. Display, automated announcement, as well as trains with air-conditioned coaches, and many more.</p>
+                        </div>
+
+                        <div class="city-box1">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLXu9bNsbC6WqRiox0sEfSzb-QBiXtrBFZGHO2CXac&s" alt="Cab Service in Surat" />
+                            <h3>Cab Service: </h3>
+                            <p>Apart from famous leading cab service providers, i.e., Ola and Uber, various other emerging cab services are popular in Surat. Prachi Taxi Services, Surat Taxi Hire, Jeevan Yatra Travellers are the most recommended by the customers
+                                and best-rated. You can easily book these cabs by either giving them a call or book through respective apps. You can select if you want an A.C. taxi or without A.C., sharing cabs or not. You can even choose the model of
+                                the car in which you want to travel. The prepaid service is a much feasible and cost-effective alternative to finding a taxi or auto-rickshaw outside the airport. Many cab service companies provide cheap, safe, reliable,
+                                and outstanding benefits. Moreover, their services remain active 24*7 and cover short-distances to long distances.
+                            </p>
+                        </div>
+
+                        <div class="city-box2"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLXu9bNsbC6WqRiox0sEfSzb-QBiXtrBFZGHO2CXac&s" alt="Air Service in Surat" />
+                            <h3>Air Service: </h3>
+                            <p>Surat International Airport is situated at Magdalla, Surat, in Gujarat, with 770 acres. It is the second busiest airport in Gujarat, following Ahmedabad in aircraft movements and passenger traffic. It has awarded as the best
+                                custom notified status on 9 June 2018. It has well strategically developed in that it links to all the major commercial centers, hospitals, research institutions, tourism destinations, and other urban centers. Thus it is
+                                a preferred airport for international tourists. </p>
+                        </div>
+
+                        <div class="city-box1">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLXu9bNsbC6WqRiox0sEfSzb-QBiXtrBFZGHO2CXac&s" alt="Metro Service Surat" />
+                            <h3>Metro Service: </h3>
+                            <p>Soon will all the metro services be initiated in Surat. It is yet under construction for a rapid transit rail system in the Gujarat state of India. It is assumed the metro project will get completed in the next three years.
+                                The undertaking of the metro project is with Gujarat Metro Rail Corporation Limited. The foundation of this project has laid by our honorable prime minister Narendra Modi on 18 January 2020. In Phase 1, twenty metro stations
+                                will be constructed on the first corridor from Sarthana to Dream city route, and 18 metro stations will be created on the second corridor from Bhesan to Saroli.
+                            </p>
+                        </div>
       </div>
     </>
   );
