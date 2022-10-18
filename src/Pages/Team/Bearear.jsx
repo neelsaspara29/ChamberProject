3f99060e7c89da13b343eef571c69fc59874e9a4
SHUBHAM <shubhamsutariya1@gmail.com>
2022-10-18 14:44:36 +0530

    latest update

--- src/Pages/Team/Bearear.jsx
+++ src/Pages/Team/Bearear.jsx
@@ -24,59 +24,8 @@ function Bearear() {
   return (
     <>
       <Header1 />
-      {/* <div className="text-danger text-center mt-10 uppercase">
-        <h1>office bearear</h1>
-      </div>
-      <div className="w-3/5 m-auto mt-5">
-        <div className="team_member">
-          <div className="d-flex flex-wrap justify-evenly">
-            {data?.map((single) => {
-              return (
-                <Card className="team_card" style={{ width: "20rem" }}>
-                  <Card.Body>
-                    <Card.Title>
-                      <div className="text-center">
-                        <img
-                          src="/Assets/team/1.jpg"
-                          // width={150}
-                          height={100}
-                          className="m-auto w-4/5"
-                        ></img>
-                      </div>
-                    </Card.Title>
-                    <Card.Subtitle className="m-auto text-muted text-center">
-                      <h4>
-                        <b className="uppercase"> {single.name}</b>
-                      </h4>
-                    </Card.Subtitle>
-                    <Card.Text>
-                      <p className="uppercase">
-                       
-                        {single.role ? single.role : " member of scci"}
-                      </p>
-                      <p>{single?.mobile}</p>
-                      <p>{single?.email}</p>
-                    </Card.Text>
-                    <Card.Link href="#">
-                      <span className="fa fa-twitter"></span>
-                    </Card.Link>
-                    <Card.Link href="#">
-                      <span className="fa fa-facebook"></span>
-                    </Card.Link>
-                    <Card.Link href="#">
-                      <span className="fa fa-instagram"></span>
-                    </Card.Link>
-                  </Card.Body>
-                </Card>
-              );
-            })}
-          </div>
-        </div>
-      </div> */}
+
       <div id="interior_content">
-        {/* <div id="interior_header_banner"><span id="bnrinst_2_450"><img src="cache/sql/fba/fs_450.jpg" width="1439" height="300" style="border:0" alt=""  /></span>
-            
-        </div> */}
         <div class="wrapper">
           <div id="content" class="rightpad pb-1">
             <div class="staffModule moduleLister ">
@@ -101,7 +50,11 @@ function Bearear() {
                             <h4>
                               <a>{item?.name}</a>
                             </h4>
-                            <h5 style={{ color: "#00945e" }}>{item?.role}</h5>
+                            <h5
+                              style={{ color: "#00945e", textAlign: "justify" }}
+                            >
+                              {item?.role}
+                            </h5>
                             <h6 class="phone" style={{ marginBottom: "1px" }}>
                               <a href="#">
                                 {item?.compneyName && (
